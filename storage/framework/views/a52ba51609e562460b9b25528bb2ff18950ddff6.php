cff59f928b101dfa9c8478605d3a30196d041c02
menan381 <menan381@gmail.com>
2020-03-31 18:24:20 +0000

    server changes

--- storage/framework/views/a52ba51609e562460b9b25528bb2ff18950ddff6.php
+++
@@ -1,7 +0,0 @@
-<?php if(count($errors) > 0): ?>
-	<div>
-		<?php $__currentLoopData = $errors; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-		<div class="alert alert-danger"><?php echo e($error); ?></div>
-		<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-	</div>
-<?php endif; ?>
